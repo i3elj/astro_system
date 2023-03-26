0b69cbb52fc1f94b95fe02fa69f35aefa9799748
i3elj <guisande.bernardo@gmail.com>
2023-03-26 16:00:41 -0300

    added functions to work with the database

--- index.php
+++ index.php
@@ -16,6 +16,19 @@ function dd($value)
     die();
 }
 
+function get_db_content()
+{
+    $database = file_get_contents("./db.json");
+    return json_decode($database, true);
+}
+
+function add_pedidos_to_db($pedido)
+{
+    $database = get_db_content();
+    array_push($database["pedidos"], $pedido);
+    file_put_contents("./db.json", json_encode($database));
+}
+
 /**
  * @param string $nome_do_prato
  * @param int $quantidade
