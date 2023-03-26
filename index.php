175017e09dfccab3487ad1ac4b1cdb6471689030
i3elj <guisande.bernardo@gmail.com>
2023-03-26 12:31:38 -0300

    first layout finished

--- index.php
+++ index.php
@@ -6,37 +6,90 @@
     <link rel="stylesheet" href="./style.css">
 </head>
 
+<?php
+function get_custo($nome_do_prato)
+{
+    return match ($nome_do_prato) {
+        "Surubim" => 128.50,
+        "Carne de Sol" => 88.79,
+        "Nego D'agua" => 60.00,
+        "Cangaceiro" => 92.50,
+        "Camarao na Moranga" => 210,
+        default => 0.00,
+    };
+}
+
+$pedidos = [
+    [
+        "nome" => "Surubim",
+        "quantidade" => 1,
+        "custo" => 128.00,
+        "hora" => "12:00",
+        "status" => "Em preparo"
+    ],
+    [
+        "nome" => "Nego d'agua",
+        "quantidade" => 1,
+        "custo" => 60.00,
+        "hora" => "12:00",
+        "status" => "Em preparo"
+    ],
+    [
+        "nome" => "Carne de Sol",
+        "quantidade" => 1,
+        "custo" => 88.00,
+        "hora" => "12:00",
+        "status" => "Em preparo"
+    ]
+];
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    array_push($pedidos, [
+        "nome" => $_POST["nome"],
+        "quantidade" => $_POST["quantidade"],
+        "custo" => get_custo($_POST["nome"]),
+        "hora" => date("H:i"),
+        "status" => "Na fila"
+    ]);
+}
+?>
+
 <body>
-    <h1>Mesa 01</h1>
-    <table>
-        <tr>
-            <th>Prato</th>
-            <th>Quant.</th>
-            <th>Preço</th>
-            <th>Horário</th>
-            <th>Status</th>
-        </tr>
-        <tr>
-            <td>Surubim</td>
-            <td>1x</td>
-            <td>R$ 128,00</td>
-            <td>12:00</td>
-            <td>Em Preparo</td>
-        </tr>
-    </table>
-    <form>
-        <div class='inputs'>
-            <div class='input-field'>
-                <label for='nome'>Nome do Prato:</label>
-                <input type='text' name='nome' />
+    <main>
+        <h1>Mesa 01</h1>
+        <table>
+            <tr>
+                <th>Prato</th>
+                <th>Quant.</th>
+                <th>Preço</th>
+                <th>Horário</th>
+                <th>Status</th>
+            </tr>
+            <?php foreach ($pedidos as $pedido) : ?>
+                <tr>
+                    <td><?= $pedido["nome"] ?></td>
+                    <td><?= $pedido["quantidade"] ?>x</td>
+                    <td>R$ <?= $pedido["custo"] ?></td>
+                    <td><?= $pedido["hora"] ?></td>
+                    <td><?= $pedido["status"] ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </table>
+
+        <form action="/" method="POST">
+            <div class='inputs'>
+                <div class='input-field'>
+                    <label for='nome'>Nome do Prato:</label>
+                    <input type='text' name='nome' />
+                </div>
+                <div id='quant-field' class='input-field'>
+                    <label for='quantidade'>Quantidade:</label>
+                    <input type='number' name='quantidade' />
+                </div>
             </div>
-            <div class='input-field'>
-                <label for='quantidade'>Quantidade:</label>
-                <input type='nuber' name='quantidade' />
-            </div>
-        </div>
-        <button>Adicionar</button>
-    </form>
+            <button type="submit">Adicionar</button>
+        </form>
+    </main>
 </body>
 
 </html>
