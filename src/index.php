8e7c091e633d82ed3c2edda381a15009129b9ffd
i3elj <guisande.bernardo@gmail.com>
2023-03-27 17:23:32 -0300

    parsed uri and notfound function

--- src/index.php
+++ src/index.php
@@ -2,7 +2,19 @@
 
 require("lib/extras.php");
 
-match ($_SERVER["REQUEST_URI"]) {
+$parsed_uri = parse_url($_SERVER["REQUEST_URI"]);
+
+match ($parsed_uri['path']) {
     '/' => require_once('views/home/index.php'),
-    default => require_once('views/notfound.php'),
+    '/pedido/editar' => require_once('views/pedido/index.php'),
+    default => notfound(),
 };
+
+function notfound()
+{
+    http_response_code(404);
+
+    require_once('views/notfound.php');
+
+    die();
+}
