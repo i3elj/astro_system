7e526664aaf19836dd2bbb42be599ce7398f618f
i3elj <guisande.bernardo@gmail.com>
2023-03-28 20:11:21 -0300

    new paths

--- src/index.php
+++ src/index.php
@@ -6,7 +6,8 @@ $parsed_uri = parse_url($_SERVER["REQUEST_URI"]);
 
 match ($parsed_uri['path']) {
     '/' => require_once('views/home/index.php'),
-    '/pedido/editar' => require_once('views/pedido/index.php'),
+    '/pedido' => dd($parsed_uri),
+    '/pedido/editar' => require_once('views/pedido/editar/index.php'),
     default => notfound(),
 };
 
