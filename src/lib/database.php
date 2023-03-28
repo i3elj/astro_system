75e5b8dbb4e8e452de72b179970a91d6db18a360
i3elj <guisande.bernardo@gmail.com>
2023-03-28 20:23:56 -0300

    new function: now you can query the db searching for a specific order with certain id

--- src/lib/database.php
+++ src/lib/database.php
@@ -4,6 +4,15 @@ function get_db_content()
     return json_decode(file_get_contents("../db.json"), true);
 }
 
+function query_by_id(int $id): array | null
+{
+    $pedidos = get_db_content()["pedidos"];
+    $found = null;
+    for ($i = 0; $i < sizeof($pedidos); $i++)
+        if ((int)($pedidos[$i])["id"] == $id) $found = $pedidos[$i];
+    return $found;
+}
+
 function add_pedidos_to_db($pedido)
 {
     $database = get_db_content();
