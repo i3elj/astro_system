788b73ad907cf2ae9e697500e76cfb7204ceeee4
i3elj <guisande.bernardo@gmail.com>
2023-04-14 13:37:51 -0300

    removed unused functions

--- views/home/home.controller.php
+++ views/home/home.controller.php
@@ -8,32 +8,6 @@ function HomeController()
     };
 }
 
-
-// TODO: this must be built in the front-end
-function add_pedido()
-{
-    $last_id = get_last_pedido_id();
-    add_pedidos_to_db([
-        "id" => is_null($last_id) ? 0 : (int)$last_id + 1,
-        "nome" => $_POST["nome"],
-        "quantidade" => $_POST["quantidade"],
-        "custo" => get_custo($_POST["nome"], (int)$_POST["quantidade"]),
-        "hora" => date("H:i"),
-        "status" => "Na fila"
-    ]);
-    build_view();
-}
-
-function get_custo(string $nome_do_prato, int $quantidade): int
-{
-    $pedidos = load_db()["pedidos"];
-    foreach ($pedidos as $pedido) {
-        if ($pedido["nome"] == $nome_do_prato) {
-            return (int)$pedido["custo"] * $quantidade;
-        }
-    }
-}
-
 function build_view()
 {
     $title = "Astro System";
