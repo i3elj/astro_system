6f2ad70e7a9895cf5455b26556baccfdc731362a
i3elj <guisande.bernardo@gmail.com>
2023-04-14 13:13:10 -0300

    new index page

---
+++ router.php
@@ -0,0 +1,45 @@
+<?php
+
+require_once 'lib/database.php';
+require_once 'lib/extras.php';
+require_once 'views/home/home.controller.php';
+
+$parsed_uri = parse_url($_SERVER["REQUEST_URI"]);
+$path = $parsed_uri['path'];
+$request_method = $_SERVER["REQUEST_METHOD"];
+
+
+$routes = [
+  [
+    'path' => '/\/ pedido \/ (\d) \/ (nome|hora)? \/?/x',
+    'controller' => 'PedidoController'
+  ],
+  [
+    'path' => '/\/home \/?/x',
+    'controller' => 'HomeController'
+  ],
+];
+
+
+foreach ($routes as $route) {
+  if (preg_match($route['path'], $path, $matches)) {
+    if ($path != $matches[0]) badrequest();
+    call_user_func($route['controller']);
+  }
+}
+
+// if the path was accepted but not found:
+notfound();
+
+function notfound()
+{
+  http_response_code(404);
+  include 'views/notfound.php';
+  exit();
+}
+
+function badrequest()
+{
+  http_response_code(400);
+  exit();
+}
