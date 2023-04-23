6361ee5f5ed8b5b6cb523dc7970bcaf9c8f76269
i3elj <guisande.bernardo@gmail.com>
2023-04-23 12:19:48 -0300

    fixed identation and new function for getting route parameters

--- lib/extras.php
+++ lib/extras.php
@@ -2,12 +2,12 @@
 
 function dd($arr_of_values)
 {
-    echo "<pre>";
-    foreach ($arr_of_values as $value) {
-        var_dump($value);
-    }
-    echo "</pre>";
-    exit();
+  echo "<pre>";
+  foreach ($arr_of_values as $value) {
+    var_dump($value);
+  }
+  echo "</pre>";
+  exit();
 }
 
 function require_style(string $file)
@@ -35,4 +35,21 @@ function badrequest()
 {
   http_response_code(400);
   exit();
+}
+
+function get_route_params(string $path, array $keys): array
+{
+  $parsed_uri = parse_url($_SERVER["REQUEST_URI"]);
+  $required_path = $parsed_uri['path'];
+
+  preg_match($path, $required_path, $matches);
+
+  $params_array = [];
+  foreach ($keys as $key) {
+    if (preg_match($key, $matches[0], $matches)) {
+      array_push($params_array, $matches[0]);
+    }
+  }
+
+  return $params_array;
 }
