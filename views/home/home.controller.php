b42b4bf2230d31e445ee879f67f8f4b801e1f47e
i3elj <guisande.bernardo@gmail.com>
2023-04-23 12:32:40 -0300

    new add_order function that uses the new model of the database

--- views/home/home.controller.php
+++ views/home/home.controller.php
@@ -13,9 +13,31 @@ function Controller($path)
   };
 }
 
+function add_order($path)
 {
+  $name = $_POST['dishName'];
+  $amount = (int) $_POST['amount'];
+  $params = get_route_params($path, ['/(\d(.+)?)/']);
+  $id = (int) $params[0];
+  $menu = Database\get_price_list();
 
+  foreach ($menu as $dish_name => $dish_price)
+    if ($dish_name == $name) $price = $dish_price;
 
+  date_default_timezone_get();
+  $order = [
+    'id' => $id,
+    'dishName' => $name,
+    'quantity' => $amount,
+    'price' => $price * $amount,
+    'hour' => date('H:i'),
+    'status' => "Na fila",
+  ];
+
+  Database\add_pedidos_to_db($order, $id);
+
+  build_view($path);
+}
 
 function build_view($path)
 {
