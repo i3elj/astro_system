82da5b2b575c28d539653fb1703cb9fb19cefdd0
i3elj <guisande.bernardo@gmail.com>
2023-04-23 12:19:31 -0300

    database.php now has a namespace

--- lib/database.php
+++ lib/database.php
@@ -1,18 +1,20 @@
 <?php
 
+namespace Database;
+
 function load_db()
 {
     return json_decode(file_get_contents("db.json"), true);
 }
 
-function get_price_list()
+function get_price_list(): array
 {
-    $menu = load_db()["menu"];
+    $menu = load_db()['restaurants'][0]["menu"];
     $price_list = [];
     foreach ($menu as $item) {
-        $price_list[$item["nome"]] = $item["custo"];
+        $price_list[$item["name"]] = $item["price"];
     }
-    return json_encode($price_list);
+    return $price_list;
 }
 
 function query_by_id(int $id): array | null
@@ -29,9 +31,14 @@ function get_last_pedido_id()
     return end(load_db()["pedidos"])["id"];
 }
 
-function add_pedidos_to_db($pedido)
+function add_pedidos_to_db(array $order, int $table_id)
 {
     $database = load_db();
-    array_push($database["pedidos"], $pedido);
-    file_put_contents("../db.json", json_encode($database));
+
+    array_push(
+        $database['restaurants'][0]['tables'][$table_id - 1]['orders'],
+        $order
+    );
+
+    file_put_contents("db.json", json_encode($database));
 }
