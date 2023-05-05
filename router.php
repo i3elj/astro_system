35a97800c302fcae6a401dac75d83c511895a606
i3elj <guisande.bernardo@gmail.com>
2023-05-05 17:44:59 -0300

    router script updated to support OOP routing

--- router.php
+++ router.php
@@ -1,24 +1,29 @@
 <?php
 
 require_once 'lib/extras.php';
-require_once 'views/home/home.controller.php';
+require_once 'controllers/home/home.controller.php';
+require_once 'controllers/auth/auth.controller.php';
 
 $parsed_uri = parse_url($_SERVER["REQUEST_URI"]);
 $path = $parsed_uri['path'];
-$request_method = $_SERVER["REQUEST_METHOD"];
 
-// the 'methods' keys are here only for documentation purposes.
 $routes = [
+  // 'path' => '/\/table\/(\d(.+)?)\/?/x',
   [
-    'path' => '/\/(table\/(\d(.+)?)\/?)?/x',
-    'controller' => 'Home\Controller',
-  ],
+    'path' => AuthController::path,
+    'handler' => 'AuthController::Handler'
+  ]
 ];
+
+if ($path == '/') HomeController::Handler();
 
 foreach ($routes as $route) {
   if (preg_match($route['path'], $path, $matches)) {
-    if ($path != $matches[0]) notfound();
-    call_user_func($route['controller'], $route['path']);
+    if ($path != $matches[0]) {
+      notfound();
+    }
+
+    $route['handler']();
   }
 }
 
