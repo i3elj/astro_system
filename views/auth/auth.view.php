2b2514cfc6fbb46fc77718d444a263c53f4ec112
i3elj <guisande.bernardo@gmail.com>
2023-05-19 12:27:57 -0300

    new dynamic content and new styles

--- views/auth/auth.view.php
+++ views/auth/auth.view.php
@@ -6,9 +6,21 @@
   <div id="loginContainer">
     <h1>Login</h1>
     <form action="/login" method="POST">
-      <input autofocus id="emailField" type="email" name="email" placeholder="Type your email" />
-      <input id="passwordField" type="password" name="password" placeholder="Type your password" />
-      <input id="submitButton" type="submit"/>
+
+      <?php
+      (isset($field) && $field == 'email') || !isset($field)
+        ? $focus = 'email'
+        : $focus = 'pwd';
+      ?>
+
+      <input required <?= $focus == 'email' ? "autofocus" : null ?> class="emailField <?= isset($auth_error) && $focus == 'email' ? 'emailFieldError' : '' ?>" type="email" name="email" placeholder="Type your email" value="<?= isset($email_recovery) ? $email_recovery : null ?>" />
+      <input required <?= $focus == 'pwd' ? 'autofocus' : null ?> class="pwdField <?= isset($auth_error) && $focus == 'pwd' ? 'pwdFieldError' : '' ?>" type="password" name="password" placeholder="Type your password" />
+      <input class="submitButton" type="submit" />
+
+      <?php if (isset($auth_error)) : ?>
+        <p id="loginError"><?= $auth_error ?></p>
+      <?php endif; ?>
+
     </form>
   </div>
 </body>
