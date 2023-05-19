4d4d4727c68c6364273b9febdacbaea5348f170c
i3elj <guisande.bernardo@gmail.com>
2023-05-19 12:40:37 -0300

    just commenting for further analysis

--- controllers/home/home.controller.php
+++ controllers/home/home.controller.php
@@ -4,8 +4,6 @@ require_once "model/home/home.model.php";
 
 class HomeController extends HomeModel
 {
-  public const path = 'Hello';
-
   static function Handler()
   {
     match ($_SERVER["REQUEST_METHOD"]) {
@@ -17,25 +15,24 @@ class HomeController extends HomeModel
 
   function add_order()
   {
-    $name = $_POST['dishName'];
-    $amount = (int) $_POST['amount'];
-    $params = get_route_params(self::path, ['/(\d(.+)?)/']);
-    $id = (int) $params[0];
-    $menu = [null]; // Database::get_price_list();
-    $this->connect();
+    // $name = $_POST['dishName'];
+    // $amount = (int) $_POST['amount'];
+    // $params = get_route_params(self::path, ['/(\d(.+)?)/']);
+    // $id = (int) $params[0];
+    // $menu = [null]; // Database::get_price_list();
 
-    foreach ($menu as $dish_name => $dish_price)
-      if ($dish_name == $name) $price = $dish_price;
+    // foreach ($menu as $dish_name => $dish_price)
+    //   if ($dish_name == $name) $price = $dish_price;
 
-    date_default_timezone_get();
-    $order = [
-      'id' => $id,
-      'dishName' => $name,
-      'quantity' => $amount,
-      'price' => $price * $amount,
-      'hour' => date('H:i'),
-      'status' => "Na fila",
-    ];
+    // date_default_timezone_get();
+    // $order = [
+    //   'id' => $id,
+    //   'dishName' => $name,
+    //   'quantity' => $amount,
+    //   'price' => $price * $amount,
+    //   'hour' => date('H:i'),
+    //   'status' => "Na fila",
+    // ];
 
     // Database::add_pedidos_to_db($order, $id);
 
@@ -44,16 +41,6 @@ class HomeController extends HomeModel
 
   static function build_view()
   {
-    // ideal: $params = get_route_params(self::path, ['id', 'name']);
-    $params = get_route_params(self::path, ['/(\d(.+)?)/']);
-    $tables = [0, 1, 2]; // Database::load_db()["restaurants"][0]["tables"];
-
-    foreach ($tables as $table) {
-      if ((int)$table["id"] == (int)$params[0]) {
-        $selected_table = $table;
-      }
-    }
-
     $title = "Astro System";
     require_style('public/style.css');
     require_style('views/home/home.style.css');
