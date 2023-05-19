d972759c5ff38ef4fbde40b90e579182d9b0a317
i3elj <guisande.bernardo@gmail.com>
2023-05-19 12:33:57 -0300

    trying to mimic errors as values in php

--- model/DatabaseModel.php
+++ model/DatabaseModel.php
@@ -2,7 +2,6 @@
 
 class DatabaseModel
 {
-
   protected function connect()
   {
     $ENV = parse_ini_file('.env');
@@ -11,7 +10,7 @@ class DatabaseModel
     $PORT = $ENV["DB_PORT"];
     $USER = $ENV["DB_USER"];
     $PWD  = $ENV["DB_PASSWORD"];
-    $NAME = $ENV["DB_NAME"];
+    $NAME = $ENV["DB_DATABASE"];
 
     $dsn =
       'pgsql:host=' . $HOST .
@@ -19,7 +18,7 @@ class DatabaseModel
       ';dbname='    . $NAME .
       ';user='      . $USER .
       ';password='  . $PWD;
-    $pdo = new PDO($dsn, $USER, $PWD);
+    $pdo = new PDO($dsn, $USER, $PWD) or throw new PDOException();
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
     return $pdo;
