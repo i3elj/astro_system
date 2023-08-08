2c15f6bd46f3845222222e06735450f00e9d054e
i3elj <guisande.bernardo@gmail.com>
2023-08-08 18:21:05 -0300

    now partials/head is a function

--- views/partials/head.php
+++ views/partials/head.php
@@ -1,4 +1,15 @@
-<head>
-  <title><?= $title ?></title>
-  <link rel="stylesheet" type="text/css" href="public/style.css">
-</head>
+<?php
+
+function create_head_tag(string $title, array $styles)
+{
+  $style_tags = "";
+  foreach ($styles as $style)
+    $style_tags = $style_tags . "<link rel='stylesheet' type='text/css' href='$style'>";
+
+  echo "<head>
+    <title>$title</title>
+    <link rel='stylesheet' type='text/css' href='public/style.css'>
+    $style_tags
+  </head>
+  ";
+}
