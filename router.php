3a844b24c7783ff2ed6abb38546b032afe8fc2ba
i3elj <guisande.bernardo@gmail.com>
2023-08-08 18:20:43 -0300

    new starting point of the program

--- router.php
+++ router.php
@@ -1,29 +1,14 @@
 <?php
 
-require_once 'lib/extras.php';
-require_once 'controllers/home/home.controller.php';
-require_once 'controllers/auth/auth.controller.php';
+function create_router(array $routes, string $real_path)
+{
+    foreach ($routes as $route) {
+        if (preg_match($route['path'], $real_path, $matches)) {
+            if ($real_path != $matches[0])
+                notfound();
 
-$parsed_uri = parse_url($_SERVER["REQUEST_URI"]);
-$path = $parsed_uri['path'];
-
-$routes = [
-  // 'path' => '/\/table\/(\d(.+)?)\/?/x',
-  [
-    'path' => AuthController::path,
-    'endpoint' => 'AuthController'
-  ]
-];
-
-if ($path == '/') HomeController::Handler();
-
-foreach ($routes as $route) {
-  if (preg_match($route['path'], $path, $matches)) {
-    if ($path != $matches[0]) {
-      notfound();
+            $view = new $route['view'];
+            $view->Handler();
+        }
     }
-
-    $endpoint = new $route['endpoint'];
-    $endpoint->Handler();
-  }
 }
