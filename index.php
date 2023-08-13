d5108d5cab39da617b3f5bfa8f9dd0d6b8a7fa31
i3elj <guisande.bernardo@gmail.com>
2023-08-13 01:24:19 -0300

    controllers are imported by an import file

--- index.php
+++ index.php
@@ -3,8 +3,7 @@
 require_once 'lib/extras.php';
 require_once 'views/partials/head.php';
 require_once 'router.php';
-require_once 'controllers/home/home.controller.php';
-require_once 'controllers/auth/auth.controller.php';
+require_once 'controllers/import.php';
 
 $parsed_uri = parse_url($_SERVER["REQUEST_URI"]);
 $path = $parsed_uri['path'];
