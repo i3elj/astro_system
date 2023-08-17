dcd7b8344c678624dfba4010387bbdee14df79b8
i3elj <guisande.bernardo@gmail.com>
2023-08-17 14:51:28 -0300

    overall development of the signup view

--- views/auth/signup/signup.js
+++ views/auth/signup/signup.js
@@ -1,7 +1,22 @@
-let signup = document.querySelector("input[type='submit']")
+import { post, handle_failed_response } from '../auth.js'
+
+let signup_field = document.querySelector("#submitButton")
 let checkbox = document.querySelector("#tos")
 
 checkbox.addEventListener('change', function () {
-    if (!this.checked) signup.disabled = true
-    else signup.disabled = false
+    if (!this.checked) signup_field.disabled = true
+    else signup_field.disabled = false
 })
+
+export async function signup() {
+    const form = document.querySelector("#form")
+    const res = await post('/signup', form)
+
+    if (!res.success) {
+        handle_failed_response(res)
+    } else {
+
+    }
+}
+
+window.signup = signup
--- views/auth/signup/signup.view.php
+++ views/auth/signup/signup.view.php
@@ -6,31 +6,88 @@
 <body>
   <div id="authContainer">
     <h1>Sign Up</h1>
-    <p></p>
-    <form id="form">
-      <input class="inputField" required type="text" name="nickname" placeholder="Choose a nickname" />
-      <input class="inputField" required type="text" name="realname" placeholder="Real (full) name" />
+    <p id="loginError">Vazio</p>
+    <form id="form" method="POST" onsubmit="signup(); return false">
+      <input
+        class="inputField"
+        type="text"
+        name="nickname"
+        placeholder="Choose a nickname"
+        required
+      />
+      <input
+        class="inputField"
+        type="text"
+        name="realname"
+        placeholder="Real (full) name"
+        required
+      />
       <div class="row">
-        <input class="inputField" required type="text" name="cpf" placeholder="CPF" />
-        <input class="inputField" required type="tel" name="phonenumber" placeholder="+55 (33)91111-2222" pattern="\+[0-9]{2} \([0-9]{2}\)[0-9]{5}-[0-9]{4}" />
+        <input
+          class="inputField"
+          type="text"
+          name="cpf"
+          placeholder="CPF ex.000.111.222-33"
+          pattern="[0-9]{3}\.[0-9]{3}\.[0-9]{3}-[0-9]{2}"
+          maxlength="14"
+          required
+        />
+        <input
+          class="inputField"
+          type="tel"
+          name="phonenumber"
+          placeholder="+55 (33)91111-2222"
+          pattern="\+[0-9]{2} \([0-9]{2}\)[0-9]{5}-[0-9]{4}"
+          required
+        />
       </div>
       <div class="row">
-        <input class="inputField" required type="email" name="email" placeholder="Type your email" />
-        <input class="inputField" required type="email" name="email" placeholder="Confirm your email" />
+        <input
+          class="inputField"
+          type="email"
+          name="email"
+          placeholder="Type your email"
+          required
+        />
+        <input
+          class="inputField"
+          type="email"
+          placeholder="Confirm your email"
+          required
+        />
       </div>
       <div class="row">
-        <input class="inputField" required type="password" name="password" placeholder="Type your password" />
-        <input class="inputField" required type="password" name="password" placeholder="Confirm your password" />
+        <input
+          class="inputField"
+          type="password"
+          name="password"
+          placeholder="Type your password"
+          required
+        />
+        <input
+          class="inputField"
+          type="password"
+          placeholder="Confirm your password"
+          required
+        />
       </div>
+
       <div id="termsOfServiceContainer">
         <input id="tos" type="checkbox" name="tos" value="yes" />
         <label for="tos">I agree with the Terms of Service</label>
       </div>
-      <input class="submitButton" type="submit" value="Sign" onclick="auth()" disabled />
+
+      <input
+        id="submitButton"
+        type="submit"
+        value="Sign"
+        disabled
+      />
     </form>
   </div>
 
-  <script type="text/javascript" src="views/auth/signup/signup.js"></script>
+  <script type="module" src="views/auth/signup/signup.js">
+  </script>
 </body>
 
 </html>
