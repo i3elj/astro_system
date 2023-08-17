7216fc5e16a1ea1d555f5036eff9b4eb4eca5957
i3elj <guisande.bernardo@gmail.com>
2023-08-17 14:55:57 -0300

    identation fix

--- controllers/home/home.controller.php
+++ controllers/home/home.controller.php
@@ -4,50 +4,47 @@ require_once "model/home/home.model.php";
 
 class HomeController extends HomeModel
 {
-  public const path = '/\//';
+    public const path = '/\//';
 
-  static function Handler()
-  {
-    match ($_SERVER["REQUEST_METHOD"]) {
-      "GET" => self::build_view(),
-      "POST" => self::add_order(),
-      default => badrequest(),
-    };
-  }
+    static function Handler()
+    {
+        match ($_SERVER["REQUEST_METHOD"]) {
+            "GET" => self::build_view(),
+            "POST" => self::add_order(),
+            default => badrequest(),
+        };
+    }
 
-  function add_order()
-  {
-    // $name = $_POST['dishName'];
-    // $amount = (int) $_POST['amount'];
-    // $params = get_route_params(self::path, ['/(\d(.+)?)/']);
-    // $id = (int) $params[0];
-    // $menu = [null]; // Database::get_price_list();
+    function add_order()
+    {
+        // $name = $_POST['dishName'];
+        // $amount = (int) $_POST['amount'];
+        // $params = get_route_params(self::path, ['/(\d(.+)?)/']);
+        // $id = (int) $params[0];
+        // $menu = [null]; // Database::get_price_list();
 
-    // foreach ($menu as $dish_name => $dish_price)
-    //   if ($dish_name == $name) $price = $dish_price;
+        // foreach ($menu as $dish_name => $dish_price)
+        //   if ($dish_name == $name) $price = $dish_price;
 
-    // date_default_timezone_get();
-    // $order = [
-    //   'id' => $id,
-    //   'dishName' => $name,
-    //   'quantity' => $amount,
-    //   'price' => $price * $amount,
-    //   'hour' => date('H:i'),
-    //   'status' => "Na fila",
-    // ];
+        // date_default_timezone_get();
+        // $order = [
+        //   'id' => $id,
+        //   'dishName' => $name,
+        //   'quantity' => $amount,
+        //   'price' => $price * $amount,
+        //   'hour' => date('H:i'),
+        //   'status' => "Na fila",
+        // ];
 
-    // Database::add_pedidos_to_db($order, $id);
+        // Database::add_pedidos_to_db($order, $id);
 
-    self::build_view();
-  }
+        self::build_view();
+    }
 
-  static function build_view()
-  {
-    $uname = isset($_SESSION['uname']) ? $_SESSION['uname'] : null;
-    $uemail = isset($_SESSION['uemail']) ? $_SESSION['uemail'] : null;
-    $title = "Astro System";
-    require_once 'views/home/home.view.php';
+    static function build_view()
+    {
+        require_once 'views/home/home.view.php';
 
-    exit(0);
-  }
+        exit(0);
+    }
 }
