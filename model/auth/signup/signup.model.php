660f90231881b67fb67b0507f0d63a17f04bf7d0
i3elj <guisande.bernardo@gmail.com>
2023-08-17 14:54:14 -0300

    two new functions for registering and checking if the user exists

--- model/auth/signup/signup.model.php
+++ model/auth/signup/signup.model.php
@@ -2,4 +2,45 @@
 
 class SignUpModel extends DatabaseModel
 {
+    protected function user_exist(string $cpf): bool
+    {
+        $stmt = $this->connect()->prepare(
+            "SELECT 1 FROM users WHERE cpf = ?"
+        );
+
+        $succeeded = $stmt->execute([$cpf]);
+
+        if (!$succeeded) {
+            printf("Prepare statement error: " . $stmt);
+            $stmt = null;
+            exit(1);
+        }
+
+        return $stmt->rowCount() == 1;
+    }
+
+    protected function register(array $user): void
+    {
+        $stmt = $this->connect()->prepare(
+            "INSERT INTO users(
+                cpf,
+                nickname,
+                real_name,
+                email,
+                password,
+                auth_token,
+                phone_number,
+                permissions,
+                role
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, 'superuser', 'owner');"
+        );
+
+        $succeeded = $stmt->execute($user);
+
+        if (!$succeeded) {
+            printf("Prepare statement error: " . $stmt);
+            $stmt = null;
+            exit(1);
+        }
+    }
 }
