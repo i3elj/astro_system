79baf14ec7d7398ee2c50c8c3655fcbe8120dd64
i3elj <guisande.bernardo@gmail.com>
2023-08-17 14:53:11 -0300

    overall development of login view

--- views/auth/login/login.view.php
+++ views/auth/login/login.view.php
@@ -6,15 +6,31 @@
 <body>
   <div id="authContainer">
     <h1>Login</h1>
-    <p></p>
-    <form id="form">
-      <input class="inputField" required type="email" name="email" placeholder="Type your email" />
-      <input class="inputField" required type="password" name="password" placeholder="Type your password" />
-      <input class="submitButton" type="submit" onclick="auth()" value="Authenticate" />
+    <p id="loginError">Vazio</p>
+    <form id="form" onsubmit="login(); return false">
+      <input
+        class="inputField"
+        type="email"
+        name="email"
+        placeholder="Type your email"
+        required
+      />
+      <input
+        class="inputField"
+        type="password"
+        name="password"
+        placeholder="Type your password"
+        required
+      />
+      <input
+        id="submitButton"
+        type="submit"
+        value="Authenticate"
+      />
     </form>
   </div>
 </body>
 
-<script type="text/javascript" src="views/auth/login/login.js"></script>
+<script type="module" src="views/auth/login/login.js"></script>
 
 </html>
