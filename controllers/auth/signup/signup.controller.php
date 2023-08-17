94246984137bfa0d5249bdf41a40d080e688a808
i3elj <guisande.bernardo@gmail.com>
2023-08-17 14:56:05 -0300

    new signup function

--- controllers/auth/signup/signup.controller.php
+++ controllers/auth/signup/signup.controller.php
@@ -10,7 +10,35 @@ class SignUpController extends SignUpModel
     {
         match ($_SERVER['REQUEST_METHOD']) {
             'GET' => self::build_view(),
+            'POST' => self::signup()
         };
+    }
+
+    private function signup()
+    {
+        $cpf = $_POST['cpf'];
+        $nickname = $_POST['nickname'];
+        $real_name = $_POST['realname'];
+        $email = $_POST['email'];
+        $phone_number = $_POST['phonenumber'];
+        $pwd = $_POST['password'];
+
+        if ($this->user_exist($cpf)) {
+            echo json_encode([
+                "success" => false,
+                "field" => 'cpf',
+                "message" => "cpf's already registered"
+            ]);
+            exit(0);
+        }
+
+        $date = date("m/d/Y h:i:s a", time());
+        $token = password_hash($cpf . $date, PASSWORD_ARGON2I);
+        $pwd = password_hash($pwd . $email, PASSWORD_ARGON2I);
+        $this->register([$cpf, $nickname, $real_name, $email, $pwd, $token, $phone_number]);
+
+        echo json_encode(["success" => true, "field" => '', "message" => '']);
+        exit(0);
     }
 
     private function build_view()
