110d3a3def920fde849d3ac3ce915b1b0014b6f2
i3elj <guisande.bernardo@gmail.com>
2023-08-23 14:47:50 -0300

    refactoring

--- views/auth/login/login.js
+++ views/auth/login/login.js
@@ -1,19 +1,10 @@
-import { post, handle_failed_response } from '../auth.js'
+import auth from '../auth.js'
 
 export async function login() {
 	const form = document.querySelector('#form')
-	const response = await post('/login', form)
+	const response = await auth.post('/login', form)
 
-	if (response.success) {
-		const token = response.token
-		const date = new Date()
-		date.setTime(date.getTime() + (7 * 24 * 60 * 60 * 1000)/*a week in ms*/)
-		document.cookie = `authToken=${token};expires=${date.toUTCString()}`
-		document.location = "http://localhost:3000/dashboard"
-		return
-	}
-
-	handle_failed_response(response)
+	response.success ? auth.on_success(response) : auth.on_error(response)
 }
 
 window.login = login
