1e4d629c7930f34a2525ae238e08fe0e4f4db410
i3elj <guisande.bernardo@gmail.com>
2023-08-23 14:46:49 -0300

    two new functions instead of just a bad one

--- views/auth/auth.js
+++ views/auth/auth.js
@@ -8,15 +8,38 @@ export async function post(path, form) {
 	return await res.json()
 }
 
-export function handle_failed_response(res) {
+export function on_success(response) {
+	const token = response.token
+	const date = new Date()
+	date.setTime(date.getTime() + (7 * 24 * 60 * 60 * 1000)/*a week in ms*/)
+	document.cookie = `authToken=${token};expires=${date.toUTCString()}`
+	document.location = "http://localhost:3000/dashboard"
+}
+
+export function on_error(response) {
 	const errorField = document.querySelector('#loginError')
-	errorField.innerText = res.message;
-	errorField.style.color = '#e38585';
-	if (res.field != undefined)
-		document.querySelector(`input[name='${res.field}']`).focus()
+	errorField.innerText = response.message
+	errorField.style.color = '#e38585'
+
+	if (response.field != undefined) {
+		let field = document.querySelector(`input[name='${response.field}']`)
+		field.classList.add("fieldError")
+		field.focus()
+		field.addEventListener("blur",
+			() => field.classList.remove("fieldError"))
+	}
 }
 
 export function logout() {
 	document.cookie = "authToken=a;expires=Thu, 01 Jan 1970 00:00:01 GMT";
 	document.location = "http://localhost:3000/login"
 }
+
+const auth = {
+	on_error,
+	on_success,
+	post,
+	logout
+}
+
+export default auth
