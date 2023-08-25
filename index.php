745b7949326d8bd32294ddf01b2158428bfa277f
i3elj <guisande.bernardo@gmail.com>
2023-08-25 00:56:06 -0300

    fixing new paths

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
-require_once 'router.php';
+require_once 'lib/router.php';
 require_once 'lib/extras.php';
 require_once 'controllers/import.php';
-require_once 'views/partials/head.php';
+require_once 'views/partials.php';
 
 $parsed_uri = parse_url($_SERVER["REQUEST_URI"]);
 $path = $parsed_uri['path'];
