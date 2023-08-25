16cde0297889c46bcbe2e42b99883b45e7916996
i3elj <guisande.bernardo@gmail.com>
2023-08-25 01:17:52 -0300

    fixed transition mistake

--- controllers/dashboard.controller.php
+++ controllers/dashboard.controller.php
@@ -19,7 +19,7 @@ class Dashboard extends DashboardModel
 	private function build_view(): void
 	{
 		$auth_token = $_COOKIE['authToken'];
-		require_once 'views/dashboard.view.php';
+		require_once 'views/dashboard/dashboard.view.php';
 		exit(0);
 	}
 }
