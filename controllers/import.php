8bf6436a5838b7f014b8a2394ddc2f2f34367d31
i3elj <guisande.bernardo@gmail.com>
2023-08-25 01:17:58 -0300

    remove testroute

--- controllers/import.php
+++ controllers/import.php
@@ -5,4 +5,3 @@ require_once 'controllers/auth/login.controller.php';
 require_once 'controllers/auth/signup.controller.php';
 require_once 'controllers/dashboard.controller.php';
 require_once 'controllers/menu.controller.php';
-require_once 'controllers/testroute/testroute.controller.php';
