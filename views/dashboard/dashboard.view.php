a67027822736a9f8251df36588d685c290ff9ffd
i3elj <guisande.bernardo@gmail.com>
2023-08-27 01:46:52 -0300

    fixed class -> namespace

--- views/dashboard/dashboard.view.php
+++ views/dashboard/dashboard.view.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
 <html>
 
-<?= Tags::head(
+<?= \Tags\head(
 	title: "Astro System - Dashboard",
 	styles: ["views/dashboard/dashboard.style.css"]
 ) ?>
 
 <body>
 	<?php if ($auth_token) : ?>
+		<?= \Tags\navbar($auth_token, $this->path) ?>
 		<h1>Hey, you're logged in! Neat</h1>
 		<button onclick="logout()">Log out</button>
 	<?php else : ?>
