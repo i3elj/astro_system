f4191e4490c459e2ebcd9e54d99fb152a558bec6
i3elj <guisande.bernardo@gmail.com>
2023-09-01 15:48:55 -0300

    created a new route array for api only now the create_router function knows if the url is for some api route

--- index.php
+++ index.php
@@ -4,18 +4,49 @@ require_once 'lib/router.php';
 require_once 'lib/extras.php';
 require_once 'controllers/import.php';
 
-$parsed_uri = parse_url($_SERVER["REQUEST_URI"]);
+$parsed_uri = parse_url($_SERVER['REQUEST_URI']);
 $path = $parsed_uri['path'];
 
+$api_routes = [
+	[
+		'path' => '/api/some/level/mesa',
+		'controller' => 'Mesas',
+		'method' => 'GET'
+	],
+	[
+		'path' => '/api/cardapio/item',
+		'controller' => 'Cardapio',
+		'method' => 'POST'
+	],
+];
+
 $routes = [
-	['path' => '/\/home/', 'controller' => 'Home'],
-	['path' => '/\/mesas/', 'controller' => 'Mesas'],
-	['path' => '/\/cardapio/', 'controller' => 'Cardapio'],
-	['path' => '/\/dashboard/', 'controller' => 'Dashboard'],
-	['path' => '/\/signup/', 'controller' => 'SignUp'],
-	['path' => '/\/login/', 'controller' => 'Login'],
+	[
+		'path' => '/home',
+		'controller' => 'Home',
+	],
+	[
+		'path' => '/mesas',
+		'controller' => 'Mesas',
+	],
+	[
+		'path' => '/cardapio',
+		'controller' => 'Cardapio',
+	],
+	[
+		'path' => '/dashboard',
+		'controller' => 'Dashboard',
+	],
+	[
+		'path' => '/signup',
+		'controller' => 'SignUp',
+	],
+	[
+		'path' => '/login',
+		'controller' => 'Login',
+	],
 	// ['path' => '/\//', 'controller' => 'LandingPage'], not implemented
 	// ['path' => '/\/caixa/', 'controller' => 'Caixa'], not implemented
 ];
 
-create_router($routes, $path);
+create_router($routes, $api_routes, $path);
--- lib/router.php
+++ lib/router.php
@@ -7,15 +7,41 @@
  * where `'path'` represents the endpoint and `'controller'` represents which
  * controller will take care of that endpoint.
  *
- *      - `[ 'path' => '/\/home/', 'controller' => 'HomeController' ]`
+ *      - `[ 'path' => '/home', 'controller' => 'HomeController' ]`
+ *
+ * @param array $api_routes Holds an associative array that have a key value
+ * pair, where `'path'` represents the endpoint and `'controller'` represents
+ * which  controller will take care of that endpoint.
+ *
+ *      - `[ 'path' => '/api/user', 'controller' => 'User', 'method' => 'GET' ]`
  *
  * @param string $path The current path the client are on.
  * @return void
  */
-function create_router($routes, $path)
+function create_router($routes, $api_routes, $path)
 {
+    if ($path == '/') {
+        header('location: /home');
+        exit(0);
+    }
+
+    $method = $_SERVER['REQUEST_METHOD'];
+
+    $contains_api = strpos($path, 'api') !== false;
+
+    if ($contains_api) {
+        foreach ($api_routes as $route) {
+            if ($route['path'] == $path) {
+                $view = new $route['controller']($path, $route['method']);
+
+                if ($method == $route['method']) $view->ApiHandler();
+                else bad_api_request();
+            }
+        }
+    }
+
     foreach ($routes as $route) {
-        if (preg_match($route['path'], $path, $matches)) {
+        if ($route['path'] == $path) {
             $view = new $route['controller']($path);
             $view->Handler();
         }
