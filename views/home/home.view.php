2e5f0bb8104db2ce28d637a61368ca5372d1c300
i3elj <guisande.bernardo@gmail.com>
2023-09-01 15:52:17 -0300

    I forgot this file out of 2325d0965132eb9d0725307e28c761793fd3a583

--- views/home/home.view.php
+++ views/home/home.view.php
@@ -1,40 +1,40 @@
 <?php
-require_once "views/partials/head.php";
-require_once "views/partials/navbar.php"
+require_once 'views/partials/head.php';
+require_once 'views/partials/navbar.php'
 ?>
 
 <!DOCTYPE html>
 <html>
 
 <?= \Tags\head(
-	title: "Astro System",
-	styles: ["views/home/home.style.css"]
+	title: 'Astro System',
+	styles: ['views/home/home.style.css']
 ) ?>
 
 <body>
 	<?= \Tags\navbar($is_logged, $this->path) ?>
-	<main class="main">
+	<main class='main'>
 		<div>
-			<h1 class="main-title">Best Restaurant System Ever!</h1>
-			<p class="main-p">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+			<h1 class='main-title'>Best Restaurant System Ever!</h1>
+			<p class='main-p'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 		</div>
 
-		<img class="main-img" src="https://wixplosives.github.io/codux-assets-storage/add-panel/image-placeholder.jpg" />
+		<img class='main-img' src='https://wixplosives.github.io/codux-assets-storage/add-panel/image-placeholder.jpg' />
 	</main>
 </body>
 
-<script type="text/javascript" src="views/home/home.js"></script>
+<script type='text/javascript' src='views/home/home.js'></script>
 
 </html>
-<!-- <div id="tableList">
+<!-- <div id='tableList'>
   <h3>Mesas</h3>
   <ul>
 	<?php // foreach ($tables as $table) :
 	?>
 	  <li>
-		<a href='/table/<?php // $table["id"]
+		<a href='/table/<?php // $table['id']
 										?>'>
-		  Mesa <?php // $table["id"]
+		  Mesa <?php // $table['id']
 						?>
 		</a>
 	  </li>
@@ -46,9 +46,9 @@ require_once "views/partials/navbar.php"
   <?php // if ($params != null) :
 	?>
 	<div>
-	  <h1 class="title">Mesa <?php // $selected_table["id"]
+	  <h1 class='title'>Mesa <?php // $selected_table['id']
 														?></h1>
-	  <table id="orderList">
+	  <table id='orderList'>
 		<tr>
 		  <th>Prato</th>
 		  <th>Quantidade</th>
@@ -58,36 +58,36 @@ require_once "views/partials/navbar.php"
 		  <th>Mais Ações</th>
 		</tr>
 
-		<?php // foreach ($selected_table["orders"] as $order) :
+		<?php // foreach ($selected_table['orders'] as $order) :
 		?>
 		  <tr>
-			<td class="table-dishName"><?php // $order["dishName"]
+			<td class='table-dishName'><?php // $order['dishName']
 																	?></td>
-			<td class="table-quantity"><?php // $order["quantity"]
+			<td class='table-quantity'><?php // $order['quantity']
 																	?>x</td>
-			<td class="table-price">R$ <?php // $order["price"]
+			<td class='table-price'>R$ <?php // $order['price']
 																	?></td>
-			<td class="table-hour"><?php // $order["hour"]
+			<td class='table-hour'><?php // $order['hour']
 															?></td>
-			<td class="table-status"><?php // $order["status"]
+			<td class='table-status'><?php // $order['status']
 																?></td>
 		  </tr>
 		<?php // endforeach
 		?>
 
 		<tr>
-		  <?php // $action_url = "/table/" . $selected_table['id'];
+		  <?php // $action_url = '/table/' . $selected_table['id'];
 			?>
 		  <form action=<?php // $action_url
-										?> method="POST">
-			<td colspan="2" class="td-input">
-			  <input id="name" type='text' name='dishName' />
+										?> method='POST'>
+			<td colspan='2' class='td-input'>
+			  <input id='name' type='text' name='dishName' />
 			</td>
-			<td colspan="2" class="td-input">
-			  <input id="amount" type='number' name='amount' />
+			<td colspan='2' class='td-input'>
+			  <input id='amount' type='number' name='amount' />
 			</td>
-			<td colspan="2" class="td-input">
-			  <button type="submit">Adicionar</button>
+			<td colspan='2' class='td-input'>
+			  <button type='submit'>Adicionar</button>
 			</td>
 		</tr>
 		</form>
