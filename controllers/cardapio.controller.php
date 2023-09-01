f1c911915a2202e6120485ca5d3b7cf94bdd8f2b
i3elj <guisande.bernardo@gmail.com>
2023-09-01 15:46:51 -0300

    check if the user is logged before getting the user info and the menu

--- controllers/cardapio.controller.php
+++ controllers/cardapio.controller.php
@@ -11,26 +11,32 @@ class Cardapio extends CardapioModel
     {
     }
 
-	/**
-	 * The main method of each controller. This method takes care of what the
-	 * controller will do depending on each http method used.
-	 */
+    /**
+     * The main method of each controller. This method takes care of what the
+     * controller will do depending on each http method used.
+     */
     public function Handler(): void
     {
         match ($_SERVER['REQUEST_METHOD']) {
             'GET' => $this->build_view(),
-            default => badrequest(),
+            default => bad_request(),
         };
     }
 
-	/**
-	 * Each Controller will have a build_view function where it sends the
-	 * desired webpage to the client.
-	 */
+    /**
+     * Each Controller will have a build_view function where it sends the
+     * desired webpage to the client.
+     */
     private function build_view(): void
     {
         $auth_token = $_COOKIE['authToken'] ?? '';
         $is_logged = $this->is_authenticated($auth_token);
+
+        if ($is_logged) {
+            $user_info = $this->getUserInfo($auth_token);
+            $menu_items = $this->getMenu($user_info['cpf']);
+        }
+
         require_once 'views/cardapio/cardapio.view.php';
         exit(0);
     }
