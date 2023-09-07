005383fb6f64e1042a50b68a21d084a6c71b0f30
i3elj <guisande.bernardo@gmail.com>
2023-09-06 22:22:56 -0300

    javascript function to expand a div to full height

---
+++ public/lib/resize.js
@@ -0,0 +1,9 @@
+window.addEventListener("resize", resize)
+window.addEventListener("load", resize)
+
+function resize(event) {
+	const mainContent = document.querySelector("#mainContent")
+	const viewport = window.innerHeight
+	const offset = mainContent.offsetTop
+	mainContent.style.height = `${viewport - offset - 20}px`
+}
