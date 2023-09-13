7c5f93922fdd7c65e15f6f0cf50a7a63046afe7e
i3elj <guisande.bernardo@gmail.com>
2023-09-13 17:58:43 -0300

    finished the base html structure and css style

--- src/views/cardapio/new/new.view.php
+++ src/views/cardapio/new/new.view.php
@@ -23,34 +23,38 @@ require_once 'src/views/partials/navbar.php';
 			<h1>Cardápio - <span>Adicionar Item</span></h1>
 		</div>
 		<div class='addItems'>
+			<a href="/cardapio" class="goBack">
+				<?= \Icons\LeftArrow() ?>
+				Voltar
+			</a>
 			<div id='mainContent'>
-				<form class='newItemContainer'>
+				<div id='newItemInfoContainer' class='newItemContainer'>
 					<div class='inputFieldWrapper'>
 						<label>Nome do Item</label>
-						<input type='text' name=''>
+						<input class='input inputFieldWrapper_input' type='text' name=''>
 					</div>
 					<div class='inputFieldWrapper'>
 						<label>Descrição</label>
-						<textarea></textarea>
+						<textarea class='input inputFieldWrapper_input'></textarea>
 					</div>
-					<input type='submit' name=''>
-				</form>
+					<button class='button inputFieldWrapper_input'>Adicionar Item</button>
+				</div>
 
 				<div class='newItemContainer'>
 					<h2>Ingredientes</h2>
 					<div id='ingredientsTable' class='tableBorder'>
 						<div class='tableRow columnsName'>
 							<div class='tableCell'>
-								<p>Nome do Ingrediente</p>
+								<p>Nome</p>
 							</div>
 							<div class='tableCell'>
 								<p>Quantidade</p>
 							</div>
 							<div class='tableCell'>
-								<p>Preco</p>
+								<p>Preço</p>
 							</div>
 							<div class='tableCell'>
-								<p>Options</p>
+								<p>Opções</p>
 							</div>
 						</div>
 						<div class='tableRow'>
@@ -65,8 +69,8 @@ require_once 'src/views/partials/navbar.php';
 								<p>5.00</p>
 							</div>
 							<div class='tableCell'>
-								<button class='button input'>Editar</button>
-								<button class='button input'>Excluir</button>
+								<button class='button'>Editar</button>
+								<button class='button'>Excluir</button>
 							</div>
 						</div>
 						<div class='tableRow'>
@@ -81,8 +85,8 @@ require_once 'src/views/partials/navbar.php';
 								<p>5.00</p>
 							</div>
 							<div class='tableCell'>
-								<button class='button input'>Editar</button>
-								<button class='button input'>Excluir</button>
+								<button class='button'>Editar</button>
+								<button class='button'>Excluir</button>
 							</div>
 						</div>
 						<div class='tableRow'>
@@ -97,8 +101,8 @@ require_once 'src/views/partials/navbar.php';
 								<p>150.00</p>
 							</div>
 							<div class='tableCell'>
-								<button class='button input'>Editar</button>
-								<button class='button input'>Excluir</button>
+								<button class='button'>Editar</button>
+								<button class='button'>Excluir</button>
 							</div>
 						</div>
 						<div class='tableRow'>
@@ -113,8 +117,8 @@ require_once 'src/views/partials/navbar.php';
 								<p>5.00</p>
 							</div>
 							<div class='tableCell'>
-								<button class='button input'>Editar</button>
-								<button class='button input'>Excluir</button>
+								<button class='button'>Editar</button>
+								<button class='button'>Excluir</button>
 							</div>
 						</div>
 					</div>
@@ -122,7 +126,7 @@ require_once 'src/views/partials/navbar.php';
 						<input class='input' type="text" name="name" placeholder='Ex: Arroz' />
 						<input class='input' type="text" name="amount" placeholder='Ex: 500ml' />
 						<input class='input' type="number" min="0" step=".01" name="price" placeholder='Ex: 15,00' />
-						<button onclick="addItem()">Adicionar</button>
+						<button class="button" onclick="addItem()">Adicionar</button>
 					</div>
 				</div>
 			</div>
