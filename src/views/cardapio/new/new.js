1c7c2bf5a8da03f3634052181e1ebadb7889b400
i3elj <guisande.bernardo@gmail.com>
2023-09-15 13:02:01 -0300

    three new functions for saving the current ingredients, adding new ingredients and request edition for an item. (under development)

--- src/views/cardapio/new/new.js
+++ src/views/cardapio/new/new.js
@@ -1,3 +1,16 @@
+function loadIngredientsInMemory() {
+	let ingredients = []
+	document.querySelectorAll('.itemRow').forEach(el => {
+		ingredients.push({
+			name: el.children[0].innerText,
+			amount: el.children[1].innerText,
+			price: el.children[2].children[1].innerText
+		})
+	})
+
+	sessionStorage.setItem('ingredients', JSON.stringify(ingredients))
+}
+
 function addItem() {
 	const name = document.querySelector('input[name="name"]')
 	const amount = document.querySelector('input[name="amount"]')
@@ -27,4 +40,52 @@ function addItem() {
 	table.innerHTML += newItem
 }
 
+/**
+ * @param {number} index The ingredient's index
+ * @return {void}
+ */
+function requestEdition(index) {
+	const inputs = {
+		name: document.querySelectorAll('input[name="name"]')[1],
+		amount: document.querySelectorAll('input[name="amount"]')[1],
+		price: document.querySelectorAll('input[name="price"]')[1]
+	}
+
+	const row = {
+		name: document.querySelectorAll('.nameValue')[index],
+		amount: document.querySelectorAll('.amountValue')[index],
+		price: document.querySelectorAll('.priceValue')[index]
+	}
+
+	inputs.name.value = row.name.innerText
+	inputs.amount.value = row.amount.innerText
+	inputs.price.value = row.price.innerText
+
+	document.querySelector('#addInputWrapper').style.display = 'none'
+	document.querySelector('#editInputWrapper').style.display = 'flex'
+
+	saveItem(index, inputs, row)
+}
+
+/**
+ * @param {number} index The ingredient's index
+ * @param {Object} inputs All inputs for editing the row
+ * @param {Object} row Elements from the row
+ * @return {void}
+ */
+function saveItem(index, inputs, row) {
+	const saveButton = document.querySelector('.saveButton')
+	saveButton.onclick = ev => {
+		row.name.innerText = inputs.name.value
+		row.amount.innerText = inputs.amount.value
+		row.price.innerText = inputs.price.value
+
+		document.querySelector('#addInputWrapper').style.display = 'flex'
+		document.querySelector('#editInputWrapper').style.display = 'none'
+	}
+}
+
+document.addEventListener('DOMContentLoaded', loadIngredientsInMemory)
+
 window.addItem = addItem
+window.requestEdition = requestEdition
