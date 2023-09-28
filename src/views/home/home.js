fb337cddaa60e955b194f76172a6d41ebd7c8b39
i3elj <guisande.bernardo@gmail.com>
2023-09-28 12:34:19 -0300

    fixing bad decisions

--- src/views/home/home.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", () => {
-	let loadScreen = document.createElement("div")
-	loadScreen.classList += "loading"
-	loadScreen.style.height = window.innerHeight + "px"
-	loadScreen.style.width = window.innerWidth + "px"
-
-	document.getElementsByTagName("body")[0].appendChild(loadScreen)
-})
-
