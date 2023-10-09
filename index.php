33c9323a8b7ea51e3f89f1a3c43c71e6e70885dc
bn <bn@bn>
2023-10-09 17:23:35 -0300

    aligned the key names

--- index.php
+++ index.php
@@ -22,13 +22,13 @@ $api_routes = [
 ];
 
 $routes = [
-	['path' => ['/', '/home'], 'controller' => '\Home\Controller'],
-	['path' => '/mesas', 'controller' => '\Mesas\Controller'],
-	['path' => '/cardapio', 'controller' => '\Cardapio\Controller'],
+	['path' => ['/', '/home'],  'controller' => '\Home\Controller'],
+	['path' => '/mesas',        'controller' => '\Mesas\Controller'],
+	['path' => '/cardapio',     'controller' => '\Cardapio\Controller'],
 	['path' => '/cardapio/new', 'controller' => '\Cardapio\New\Controller'],
-	['path' => '/dashboard', 'controller' => '\Dashboard\Controller'],
-	['path' => '/signup', 'controller' => '\Signup\Controller'],
-	['path' => '/login', 'controller' => '\Login\Controller'],
+	['path' => '/dashboard',    'controller' => '\Dashboard\Controller'],
+	['path' => '/signup',       'controller' => '\Signup\Controller'],
+	['path' => '/login',        'controller' => '\Login\Controller'],
 ];
 
 create_router($routes, $api_routes, $path, $static_routes, $file_types);
