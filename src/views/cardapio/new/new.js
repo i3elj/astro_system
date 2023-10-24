bec93551b1c0604de87c301f9125f10222cd1363
i3elj <guisande.bernardo@gmail.com>
2023-10-24 16:55:13 -0300

    barely working code for editing and adding new ingredients to new menu item

--- src/views/cardapio/new/new.js
+++ src/views/cardapio/new/new.js
@@ -1,91 +1,106 @@
+/*
+NOTES:
+	every ingredient cell must have an eventListener to onchange event. Whenever
+	the value changes the 'ingredientes' storage item should be updated at the
+	correct location.
+*/
+
+/**
+ * @param className {string}
+ * @param fn {function(Element, number, NodeListOf<Element>): void}
+ */
+function mapOverHTML(className, fn) {
+	document.querySelectorAll(className).forEach((v, k, p) => fn(v, k, p))
+}
+
+/**
+ * Add eventListeners to every ingredient input field so everytime the user
+ * decides to change any ingredient, the memory will be updated.
+ *
+ * TODO: it can be improved by adding the event listener only when first loading
+ * the page and when adding a ingredient to the table.
+ */
+function addEventListenersToInputField() {
+	mapOverHTML('.item_row', function (el, k, p) {
+		let row = el.children
+		for (let i = 0; i < row.length - 1; i++) {
+			let inputField = row[i].children[0]
+			inputField.addEventListener('input', loadIngredientsInMemory)
+		}
+	})
+}
+
+/**
+ * Traverse every ingredient and loads it into memory
+ */
 function loadIngredientsInMemory() {
 	let ingredients = []
-	document.querySelectorAll('.itemRow').forEach(el => {
+
+	mapOverHTML('.item_row', function (el) {
 		ingredients.push({
-			name: el.children[0].innerText,
-			amount: el.children[1].innerText,
-			price: el.children[2].children[1].innerText
+			name: el.children[0].children[0].value,
+			amount: el.children[1].children[0].value,
+			price: el.children[2].children[0].value
 		})
 	})
 
 	sessionStorage.setItem('ingredients', JSON.stringify(ingredients))
 }
 
-function addItem() {
-	const name = document.querySelector('input[name="name"]')
-	const amount = document.querySelector('input[name="amount"]')
-	const price = document.querySelector('input[name="price"]')
+/**
+ * Returns the json equivalent of loaded ingredients in memory
+ */
+function loadIngredientsOffMemory() {
+	return JSON.parse(sessionStorage.getItem('ingredients'))
+}
 
-	const table = document.getElementById('ingredientsTable')
+/**
+ * Adds an ingredient to memory json object
+ */
+function addIngredientToMemory(n, a, p) {
+	let ingredients = loadIngredientsOffMemory()
+	ingredients.push({ name: n, amount: a, price: p })
+	sessionStorage.setItem('ingredients', JSON.stringify(ingredients))
+}
+
+/**
+ * Adds an ingredient to html table and memory
+ */
+window.addItem = function () {
+	let ingredients = loadIngredientsOffMemory()
+
+	const name = document.querySelector('input[name="name"]').value
+	const amount = document.querySelector('input[name="amount"]').value
+	const price = document.querySelector('input[name="price"]').value
+	const table = document.getElementById('ingredients_table')
+	const index = ingredients.length
 
 	const newItem = `
-		<div class='tableRow'>
-			<div class='tableCell'>
-				<p>${name.value}</p>
-			</div>
-			<div class='tableCell'>
-				<p>${amount.value}</p>
-			</div>
-			<div class='tableCell'>
-				<p>R$ </p>
-				<p>${price.value}</p>
-			</div>
-			<div class='tableCell'>
-				<button class='button'>Editar</button>
-				<button class='button'>Excluir</button>
-			</div>
+		<div class='table_row item_row'>
+			<div class='table_cell'>
+				<input class='name_value' value='${name}'/></div>
+			<div class='table_cell'>
+				<input class='amount_value' value='${amount}'></div>
+			<div class='table_cell'>
+				<input class='price_value' value='${price}'/></div>
+			<div class='table_cell'>
+				<button class='button'
+					onclick='deleteItem(${index})'>Excluir</button></div>
 		</div>
 	`
 
+	addIngredientToMemory(name, amount, price)
 	table.innerHTML += newItem
+	addEventListenersToInputField()
 }
 
-/**
- * @param {number} index The ingredient's index
- * @return {void}
- */
-function requestEdition(index) {
-	const inputs = {
-		name: document.querySelectorAll('input[name="name"]')[1],
-		amount: document.querySelectorAll('input[name="amount"]')[1],
-		price: document.querySelectorAll('input[name="price"]')[1]
-	}
+// TODO: implement
+window.saveItem = () => undefined
 
-	const row = {
-		name: document.querySelectorAll('.nameValue')[index],
-		amount: document.querySelectorAll('.amountValue')[index],
-		price: document.querySelectorAll('.priceValue')[index]
-	}
+document.addEventListener('DOMContentLoaded', function () {
+	loadIngredientsInMemory()
+	addEventListenersToInputField()
+})
 
-	inputs.name.value = row.name.innerText
-	inputs.amount.value = row.amount.innerText
-	inputs.price.value = row.price.innerText
-
-	document.querySelector('#addInputWrapper').style.display = 'none'
-	document.querySelector('#editInputWrapper').style.display = 'flex'
-
-	saveItem(index, inputs, row)
-}
-
-/**
- * @param {number} index The ingredient's index
- * @param {Object} inputs All inputs for editing the row
- * @param {Object} row Elements from the row
- * @return {void}
- */
-function saveItem(index, inputs, row) {
-	const saveButton = document.querySelector('.saveButton')
-	saveButton.onclick = ev => {
-		row.name.innerText = inputs.name.value
-		row.amount.innerText = inputs.amount.value
-		row.price.innerText = inputs.price.value
-
-		document.querySelector('#addInputWrapper').style.display = 'flex'
-		document.querySelector('#editInputWrapper').style.display = 'none'
-	}
-}
-
-document.addEventListener('DOMContentLoaded', loadIngredientsInMemory)
-
-window.addItem = addItem
-window.requestEdition = requestEdition
+let intervalId = setInterval(loadIngredientsInMemory, 10000)
+document.addEventListeners('beforeunload', clearInterval(intervalId))
