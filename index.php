dde0d2a00e893823aab041ef8923b8f5e6d8ddb1
i3elj <guisande.bernardo@gmail.com>
2023-10-24 16:01:14 -0300

    static routes added to main router function

--- index.php
+++ index.php
@@ -3,8 +3,9 @@
 require_once 'lib/router.php';
 require_once 'lib/extras.php';
 require_once 'src/controllers/import.php';
+require_once 'src/apis/import.php';
 
-$parsed_uri = parse_url($_SERVER['REQUEST_URI']);
+$parsed_uri = parse_url($_SERVER['REQUEST_URI']); // implement my own parser
 $path = $parsed_uri['path'];
 $file_types = get_filetype_regex('css', 'js', 'png');
 
@@ -31,4 +32,4 @@ $routes = [
 	['path' => '/login',        'controller' => '\Login\Controller'],
 ];
 
-create_router($routes, $api_routes, $path, $static_routes, $file_types);
+create_router($routes, $api_routes, $static_routes, $path);
