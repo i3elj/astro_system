d2bf95c7faae0a02e2048139f995c81065016ec4
i3elj <guisande.bernardo@gmail.com>
2023-10-24 16:03:21 -0300

    fixed class name

--- public/lib/resize.js
+++ public/lib/resize.js
@@ -1,9 +1,9 @@
-window.addEventListener("resize", resize)
-window.addEventListener("load", resize)
-
 function resize(event) {
-	const mainContent = document.querySelector("#mainContent")
+	const mainContent = document.querySelector("#content_wrapper")
 	const viewport = window.innerHeight
 	const offset = mainContent.offsetTop
-	mainContent.style.height = `${viewport - offset - 22}px`
+	mainContent.style.height = `${viewport - offset - 42}px`
 }
+
+window.addEventListener("resize", resize)
+window.addEventListener("load", resize)
